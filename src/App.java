05cda0e84e518ae13b905b781815d1869828473a
ShaynaHayes <28shayes@gannacademy.org>
2025-02-10 13:15:29 -0500

    Finished

--- src/App.java
+++ src/App.java
@@ -1,30 +1,54 @@
 import java.util.*;
 
 /**
- *  Task: Write code that will take input and output different messages. 
- *  
- *  Description: Our code should take in inputs then print a message based on divisibility tests.
+ * Task: Write code that will take input and output different messages.
  * 
- *  Basic task (up to B+ level): Take input from the user. Output if the number is even or odd.
- *  
- *  Moderate task (up to A level): Do all of the above AND check to see if the number is divisible 
- *  by 3 and/or 5:
- *  If the number is divisible by 3, print "The number is divisible by 3."
- *  If the number is divisible by 5, print "The number is divisible by 5."
- *  If divisible by both 3 and 5, print "The number is divisible by both 3 and 5."
+ * Description: Our code should take in inputs then print a message based on
+ * divisibility tests.
  * 
- *  Challenge task (up to A+ level): Complete both of the above AND check to see if the number 
- *  is greater than 0 and less than 100. If the number is less than 0 or above 100, say "Wrong number!".
- *  If the number is between 0 and 100, check to see if it is a prime number. A prime number less than
- *  100 should be divisible by itself and 1 but not by 2, 3, 5, or 7.
- *  
+ * Basic task (up to B+ level): Take input from the user. Output if the number
+ * is even or odd.
+ * 
+ * Moderate task (up to A level): Do all of the above AND check to see if the
+ * number is divisible
+ * by 3 and/or 5:
+ * If the number is divisible by 3, print "The number is divisible by 3."
+ * If the number is divisible by 5, print "The number is divisible by 5."
+ * If divisible by both 3 and 5, print "The number is divisible by both 3 and
+ * 5."
+ * 
+ * Challenge task (up to A+ level): Complete both of the above AND check to see
+ * if the number
+ * is greater than 0 and less than 100. If the number is less than 0 or above
+ * 100, say "Wrong number!".
+ * If the number is between 0 and 100, check to see if it is a prime number. A
+ * prime number less than
+ * 100 should be divisible by itself and 1 but not by 2, 3, 5, or 7.
+ * 
  */
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number");
+        int value = Integer.valueOf(scanner.nextLine());
 
+        if (value % 2 == 0) {
+            System.out.println("Your number is even");
+        } else {
+            System.out.println("Your value is odd");
+        }
 
-        
+        if (value % 3 == value % 5) {
+            System.out.println("Your number is divisible by both 3 and 5.");
+        } else if (value % 3 == 0) {
+            System.out.println("The number is divisible by 3.");
+        }
+
+        else if (value % 5 == 0 ) {
+            System.out.println("The number is divisible by 5.");
+        }
+
     }
+
 }
